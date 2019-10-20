fc831e6ad23b5016841815947974dc2dd209d060
Sukhorukov Maksim <sukhorukov.mv@reliab.tech>
2019-10-20 14:10:29 +0300

    create new directory for game

---
+++ src/games/brain-calc.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace BrainGames\Games;
+
+use function \BrainGames\Engine\runGame;
+use function \BrainGames\Cli\viewRules;
+use function \BrainGames\Cli\viewRuleBrainEven;
+use function \BrainGames\Cli\askQuestion;
+
+function getCorrectAnswerBrainCalc()
+{
+    $rand1 = rand(1, 100);
+    $rand2 = rand(1, 100);
+    $operators = array('*', '/', '+', '-');
+    $randoperator = $operators[rand(0, 3)];
+
+    $question = "${rand1} ${randoperator} ${rand2}";
+
+    askQuestion($question);
+
+    switch ($randoperator) {
+    case "+":
+        $correctAnswer = $rand1 + $rand2;
+        break;
+    case "-":
+        $correctAnswer = $rand1 - $rand2;
+        break;
+    case "*":
+        $correctAnswer = $rand1 * $rand2;
+        break;
+    case "/":
+        $correctAnswer = $rand1 / $rand2;
+        break;
+    }
+
+    return strval($correctAnswer);
+}
+
+function runBrainCalc()
+{
+    $getCorrect = '\BrainGames\Games\getCorrectAnswerBrainCalc';
+    $viewRules = '\BrainGames\Cli\viewRuleBrainCalc'; 
+    runGame($getCorrect, $viewRules);
+}
---
+++ src/games/brain-even.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace BrainGames\Games;
+
+use function \BrainGames\Engine\runGame;
+use function \BrainGames\Cli\viewRules;
+use function \BrainGames\Cli\viewRuleBrainEven;
+use function \BrainGames\Cli\askQuestion;
+
+function checkEven($num)
+{
+    $divisor = 2;
+    if ($num % $divisor == 0) {
+        return "yes";
+    } else {
+        return "no";
+    }
+}
+
+function getCorrectAnswerBranEven()
+{
+    $randomNumber = rand(1, 100);
+    askQuestion($randomNumber);
+    return checkEven($randomNumber);
+}
+
+function runBrainEven()
+{
+    $getCorrect = '\BrainGames\Games\getCorrectAnswerBranEven';
+    $viewRules = '\BrainGames\Cli\viewRuleBrainEven'; 
+    runGame($getCorrect, $viewRules);
+}
